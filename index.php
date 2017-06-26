4d61caa322621fd97794a40559b89313281731dc
Brothers In Web <brothersinweb@gmail.com>
2017-06-26 15:09:32 -0700

    update

--- index.php
+++ index.php
@@ -11,40 +11,43 @@
 
 <body>
 <header>
-	<h1>Brothersinweb</h1>
-	<h2>Udji u svet interneta na velika vrata</h2>
+	<div>
+		<h1>Brothersinweb</h1>
+		<h2>Udji u svet interneta na velika vrata</h2>
+	</div>
 </header>
 	<div class="first">
+
 		<p>slajdhflksajdflk<br />
 			<small>lkasjdflaksjdflkasjdlfkasdfs dkjanfsdl fsadjnfa sdfj asdjfnaskndf sadf oaskd fpkasdf</small></p>
+		<img src="slike/arrow-down.png" alt="arrow-down" class="arrow-down">
 	</div>
 
 	<div class="second">
 		<p>i tako<br />
 			<small>lkjsdfoiownefowaienf f aoe fwefoawejfnoiawef wef oawie fwa efiawejfiwajefia fj </small></p>
+			<img src="slike/arrow-down.png" alt="arrow-down" class="arrow-down">
 	</div>
 
 	<div class="third">
 		<p>kajsdnfadefmoaskdfmlkm<br />
 			<small>jnadoigjsaidkfnsj fdasjnfoasn fas foiuasnjfikjasldf </small></p>
+			<img src="slike/arrow-down.png" alt="arrow-down" class="arrow-down">
 	</div>
 	
 <footer>
-	<div id="first-footer">
+	<div id="first-div">
 		<div id="second-div">
-			<div id="third-div">
-				<div id="fourth-div">
+			
 				    <form method="post">
 				        <label for="name">Ime:</label><br/><input type="text" name="name" placeholder="Ime"/><br/><br/>
 				        
 				        <label for="email">Email:</label><br/><input type="email" name="email" placeholder="vas@mail.com"/><br/><br/>
 				        
 				        <label for="message">Poruka:</label><br/>
-				        <textarea  name="message" rows="16" cols="50"></textarea>
-				        <input type="submit" name="submit" value="Posalji">
+				        <textarea  name="message" rows="16" cols="50"></textarea><br>
+				        <button type="submit" name="submit">Posalji</button>
 				    </form>	
-				</div>
-			</div>
 		</div>
 	</div>
 </footer>
--- js/index.js
+++ js/index.js
@@ -7,4 +7,41 @@ $(document).ready(function(){
   });
 
 
+
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+        $('#sticky-anchor').height($('#sticky').outerHeight());
+    } else {
+        $('#sticky').removeClass('stick');
+        $('#sticky-anchor').height(0);
+    }
+}
+
+$(function() {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
+
+var dir = 1;
+var MIN_TOP = 20;
+var MAX_TOP = 35;
+
+function autoscroll() {
+    var window_top = $(window).scrollTop() + dir;
+    if (window_top >= MAX_TOP) {
+        window_top = MAX_TOP;
+        dir = -1;
+    } else if (window_top <= MIN_TOP) {
+        window_top = MIN_TOP;
+        dir = 1;
+    }
+    $(window).scrollTop(window_top);
+    window.setTimeout(autoscroll, 100);
+}
+
+
+
 })
