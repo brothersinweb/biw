e0dc954782fe06c9949caa62e01e74e8d8d40d06
Brothers In Web <brothersinweb@gmail.com>
2017-06-26 09:15:36 -0700

    update

--- index.php
+++ index.php
@@ -1,37 +1,48 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <script src="jquery.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<link href='https://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-
+	<title>BIW</title>
+	<script src="jquery.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<link href='https://fonts.googleapis.com/css?family=Londrina+Shadow' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
-<h1>Brothersinweb</h1>
-<h2>Udji u svet interneta na velika vrata</h2>
-<div class="first">
-  <p>slajdhflksajdflk<br />
-  <small>lkasjdflaksjdflkasjdlfkasdfs dkjanfsdl fsadjnfa sdfj asdjfnaskndf sadf oaskd fpkasdf</small></p>
-</div>
+<header>
+	<h1>Brothersinweb</h1>
+	<h2>Udji u svet interneta na velika vrata</h2>
+</header>
+	<div class="first">
+		<p>slajdhflksajdflk<br />
+			<small>lkasjdflaksjdflkasjdlfkasdfs dkjanfsdl fsadjnfa sdfj asdjfnaskndf sadf oaskd fpkasdf</small></p>
+	</div>
 
-<div class="second">
-  <p>i tako<br />
-  <small>lkjsdfoiownefowaienf f aoe fwefoawejfnoiawef wef oawie fwa efiawejfiwajefia fj </small></p>
-</div>
-  
-<div class="dessert">
-  <p>kajsdnfadefmoaskdfmlkm<br />
-  <small>jnadoigjsaidkfnsj fdasjnfoasn fas foiuasnjfikjasldf </small></p>
-</div>
+	<div class="second">
+		<p>i tako<br />
+			<small>lkjsdfoiownefowaienf f aoe fwefoawejfnoiawef wef oawie fwa efiawejfiwajefia fj </small></p>
+	</div>
 
+	<div class="third">
+		<p>kajsdnfadefmoaskdfmlkm<br />
+			<small>jnadoigjsaidkfnsj fdasjnfoasn fas foiuasnjfikjasldf </small></p>
+	</div>
+	
+<footer>
+	<div>
+	    <form method="post">
+	        <label for="name">Ime:</label><br/><input type="text" name="name" placeholder="Ime"/><br/><br/>
+	        
+	        <label for="email">Email:</label><br/><input type="email" name="email" placeholder="vas@mail.com"/><br/><br/>
+	        
+	        <label for="message">Poruka:</label><br/>
+	        <textarea  name="message" rows="20" cols="70"></textarea>
+	        <input type="submit" name="submit" value="Posalji">
+	    </form>
+	</div>
+</footer>
 
-
-<script type="text/javascript" src="js/index.js"></script>
+				<script type="text/javascript" src="js/index.js"></script>
 </body>
 </html>
